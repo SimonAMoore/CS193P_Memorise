b39d8a9d123ce2c40fc95e0004715799e8ddf705
Simon Moore <simonaamoore@hotmail.com>
2025-03-31 16:53:12 +0100

    Refactored concatenation and shuffling of emojis array

---
+++ Memorise/ContentView.swift
@@ -0,0 +1,88 @@
+//
+//  ContentView.swift
+//  Memorise
+//
+//  Created by Simon Moore on 29/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var emojis: Array<String> = []
+    
+    var body: some View {
+        VStack {
+            Text("Memorise!").font(.largeTitle)
+            ScrollView {
+                cards
+            }
+            Spacer()
+            themeChoosingButtons
+        }
+        .padding()
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 60))]) {
+            ForEach(emojis.indices, id: \.self) { index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+        }
+        .foregroundColor(.orange)
+    }
+    
+    func themeChoosingButton(emojis content: Array<String>, title: String, symbol: String) -> some View {
+        Button(action: {
+            emojis = [content, content].flatMap( { $0 } ).shuffled()
+        }, label: {
+            VStack {
+                Image(systemName: symbol).font(.largeTitle)
+                Text(title)
+            }
+        })
+    }
+    
+    var themeChoosingButtons: some View {
+        HStack(alignment: .lastTextBaseline) {
+            Spacer()
+            themeChoosingButton(emojis: ["👻", "🎃", "🕷️", "😈", "💀", "🕸️", "🧙", "🙀", "👹", "😱", "☠️", "🍭"], title: "Halloween", symbol: "brain.fill")
+            Spacer()
+            themeChoosingButton(emojis: ["✈️", "🚗", "🚀", "🚘", "🚙", "🚎", "🚛", "🚕", "🚐", "🚁", "🚜", "⛵️", "🚒", "🏎️", "🏍️", "🚔"], title: "Vehicles", symbol: "car.fill")
+            Spacer()
+            themeChoosingButton(emojis: ["🐱", "🐈", "🐶", "🐿️", "🦊", "🐷", "🐮", "🐼", "🦆", "🦦"], title: "Animals", symbol: "pawprint.fill")
+            Spacer()
+        }
+    }
+}
+
+struct CardView: View {
+    @State var isFaceUp = false
+
+    var cornerRadius    = 16.0
+    var lineWidth       = 4.0
+
+    let content: String
+
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: cornerRadius)
+            
+            Group {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: lineWidth)
+                Text(content).font(.largeTitle)
+            }
+            .opacity(isFaceUp ? 1 : 0)
+            
+            base.fill().opacity(isFaceUp ? 0 : 1)
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
