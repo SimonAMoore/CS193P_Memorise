aa64f8ac04a8cfd0613c1d4cb02dcf5f3be4b835
Simon Moore <simonaamoore@hotmail.com>
2025-03-31 12:43:19 +0100

    Added isFaceUp, cornerRadius, lineWidth vars to CardView

--- Memorise/ContentView.swift
+++ Memorise/ContentView.swift
@@ -9,13 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
+        .foregroundColor(.orange)
         .padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    var cornerRadius: CGFloat = 16
+    var lineWidth: CGFloat = 4
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: cornerRadius).foregroundColor(.white)
+                RoundedRectangle(cornerRadius: cornerRadius).strokeBorder(lineWidth: lineWidth)
+                Text("👻").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: cornerRadius)
+            }
+        }
     }
 }
 
